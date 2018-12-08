396460a1c2c36cf4dd3de21904c4162c1455fae0
Jakub Kozłowski <kubukoz@gmail.com>
2018-12-08 12:12:49 +0100

    Add Duration CommutativeMonoid

--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/DurationTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/DurationTests.scala
@@ -1,9 +1,9 @@
 package io.chrisdavenport.cats.time.instances
 
-
 import cats.tests.CatsSuite
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
+import cats.kernel.laws.discipline.CommutativeMonoidTests
 import TimeArbitraries._
 import java.time.Duration
 import io.chrisdavenport.cats.time.instances.duration._
@@ -11,4 +11,5 @@ import io.chrisdavenport.cats.time.instances.duration._
 class DurationTests extends CatsSuite {
   checkAll("Duration", HashTests[Duration].hash)
   checkAll("Duration", OrderTests[Duration].order)
+  checkAll("Duration", CommutativeMonoidTests[Duration].commutativeMonoid)
 }
