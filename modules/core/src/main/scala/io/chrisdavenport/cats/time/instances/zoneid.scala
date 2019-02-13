997c774cf9bb677ace3a678301ebae15a485b3d3
Dmitry Polienko <dmitry@eldis.ru>
2019-02-13 11:16:32 +0700

    Add Eq[ZoneId], Hash[ZoneId]

--- modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/zoneid.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/zoneid.scala
@@ -5,7 +5,9 @@ import java.time.ZoneId
 
 trait zoneid {
   implicit final val zoneidInstances = 
-    new Show[ZoneId]{
+    new Show[ZoneId] with Eq[ZoneId] with Hash[ZoneId]{
+      override def eqv(x: ZoneId, y: ZoneId): Boolean = x.equals(y)
+      override def hash(x: ZoneId): Int = x.hashCode
       override def show(x: ZoneId): String = x.getId
     }
 }
--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/ZoneIdTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/ZoneIdTests.scala
@@ -2,13 +2,13 @@ package io.chrisdavenport.cats.time.instances
 
 
 import cats.tests.CatsSuite
-// import cats.kernel.laws.discipline.HashTests
-// import cats.kernel.laws.discipline.OrderTests
-// import TimeArbitraries._
-// import java.time.ZoneId
-// import io.chrisdavenport.cats.time.instances.hashWithOrder._
+import cats.kernel.laws.discipline.HashTests
+import cats.kernel.laws.discipline.EqTests
+import TimeArbitraries._
+import java.time.ZoneId
+import io.chrisdavenport.cats.time.instances.zoneid._
 
 class ZoneIdTests extends CatsSuite {
-  // checkAll("ZoneId", HashTests[ZoneId].hash)
-  // checkAll("ZoneId", OrderTests[ZoneId].order)
+  checkAll("ZoneId", EqTests[ZoneId].eqv)
+  checkAll("ZoneId", HashTests[ZoneId].hash)
 }
