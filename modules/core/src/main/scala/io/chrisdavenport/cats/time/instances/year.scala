bc5ccbd071dc0f59c1ef440d48e5cda12a9eb442
JesusMtnez <jesusmartinez93@gmail.com>
2019-02-22 07:39:00 +0100

    Add Eq[Year]

--- modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/year.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/year.scala
@@ -6,10 +6,11 @@ import java.time.format.DateTimeFormatter
 
 trait year {
   implicit final val yearInstances =
-    new Show[Year] with Order[Year] with Hash[Year]{
+    new Show[Year] with Order[Year] with Hash[Year] with Eq[Year]{
       override def hash(x: Year): Int = x.hashCode
       override def compare(x: Year, y: Year): Int = x.compareTo(y)
       override def show(x: Year): String = x.format(DateTimeFormatter.ofPattern("yyyy"))
+      override def eqv(x: Year, y: Year): Boolean = x.equals(y)
     }
 }
 
--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/YearTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/YearTests.scala
@@ -1,6 +1,7 @@
 package io.chrisdavenport.cats.time.instances
 
 import cats.tests.CatsSuite
+import cats.kernel.laws.discipline.EqTests
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
 import TimeArbitraries._
@@ -8,6 +9,7 @@ import java.time.Year
 import io.chrisdavenport.cats.time.instances.year._
 
 class YearTests extends CatsSuite {
+  checkAll("Year", EqTests[Year].eqv)
   checkAll("Year", HashTests[Year].hash)
   checkAll("Year", OrderTests[Year].order)
 }
