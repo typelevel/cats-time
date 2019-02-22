75268b4d115da165abd4d53fb7915c6dbbed784f
JesusMtnez <jesusmartinez93@gmail.com>
2019-02-22 07:56:08 +0100

    Add Eq[ZoneOffset]

--- modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/zoneoffset.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/zoneoffset.scala
@@ -5,10 +5,11 @@ import java.time.ZoneOffset
 
 trait zoneoffset {
   implicit final val zoneoffsetInstances =
-    new Show[ZoneOffset] with Order[ZoneOffset] with Hash[ZoneOffset]{
+    new Show[ZoneOffset] with Order[ZoneOffset] with Hash[ZoneOffset] with Eq[ZoneOffset]{
       override def hash(x: ZoneOffset): Int = x.hashCode
       override def compare(x: ZoneOffset, y: ZoneOffset): Int = x.compareTo(y)
       override def show(x: ZoneOffset): String = x.getId
+      override def eqv(x: ZoneOffset, y: ZoneOffset): Boolean = x.equals(y)
     }
 }
 
--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/ZoneOffsetTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/ZoneOffsetTests.scala
@@ -1,14 +1,16 @@
 package io.chrisdavenport.cats.time.instances
 
+import java.time.ZoneOffset
 
 import cats.tests.CatsSuite
+import cats.kernel.laws.discipline.EqTests
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
 import TimeArbitraries._
-import java.time.ZoneOffset
 import io.chrisdavenport.cats.time.instances.zoneoffset._
 
 class ZoneOffsetTests extends CatsSuite {
+  checkAll("ZoneOffset", EqTests[ZoneOffset].eqv)
   checkAll("ZoneOffset", HashTests[ZoneOffset].hash)
   checkAll("ZoneOffset", OrderTests[ZoneOffset].order)
 }
