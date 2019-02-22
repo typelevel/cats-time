27fca83bc0584cd2add1c0cc88e97c7a274bd75e
JesusMtnez <jesusmartinez93@gmail.com>
2019-02-22 07:51:00 +0100

    Add Eq[MonthDay]

--- modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/monthday.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/monthday.scala
@@ -3,16 +3,15 @@ package io.chrisdavenport.cats.time.instances
 import java.time.MonthDay
 import java.time.format.DateTimeFormatter
 
-import cats.{ Hash, Order, Show }
+import cats._
 
 trait monthday {
   implicit final val monthdayInstances =
-    new Show[MonthDay] with Order[MonthDay] with Hash[MonthDay] {
-      def compare(x: MonthDay, y: MonthDay): Int = x.compareTo(y)
-
-      def hash(x: MonthDay): Int = x.hashCode
-
-      def show(x: MonthDay): String = x.format(DateTimeFormatter.ofPattern("M-d"))
+    new Show[MonthDay] with Order[MonthDay] with Hash[MonthDay] with Eq[MonthDay] {
+      override def compare(x: MonthDay, y: MonthDay): Int = x.compareTo(y)
+      override def hash(x: MonthDay): Int = x.hashCode
+      override def show(x: MonthDay): String = x.format(DateTimeFormatter.ofPattern("M-d"))
+      override def eqv(x: MonthDay, y: MonthDay): Boolean = x.equals(y)
     }
 }
 
--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/MonthDayTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/MonthDayTests.scala
@@ -2,12 +2,15 @@ package io.chrisdavenport.cats.time.instances
 
 import java.time.MonthDay
 
-import cats.kernel.laws.discipline.{ HashTests, OrderTests }
 import cats.tests.CatsSuite
-import io.chrisdavenport.cats.time.instances.monthday._
+import cats.kernel.laws.discipline.EqTests
+import cats.kernel.laws.discipline.HashTests
+import cats.kernel.laws.discipline.OrderTests
 import TimeArbitraries._
+import io.chrisdavenport.cats.time.instances.monthday._
 
 class MonthDayTests extends CatsSuite {
+  checkAll("MonthDay", EqTests[MonthDay].eqv)
   checkAll("MonthDay", HashTests[MonthDay].hash)
   checkAll("MonthDay", OrderTests[MonthDay].order)
 }
