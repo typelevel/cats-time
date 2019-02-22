4da7a1579babcb5cecca0e4803442ac6540e45df
JesusMtnez <jesusmartinez93@gmail.com>
2019-02-22 07:57:46 +0100

    Add Eq[OffsetDateTime]

--- modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/offsetdatetime.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cats/time/instances/offsetdatetime.scala
@@ -10,11 +10,12 @@ trait offsetdatetime {
   final def showOffsetDateTime(formatter: DateTimeFormatter): Show[OffsetDateTime] =
     Show[String].contramap(_.format(formatter))
 
-  implicit final val offsetdatetimeInstances = 
-    new Show[OffsetDateTime] with Order[OffsetDateTime] with Hash[OffsetDateTime]{
+  implicit final val offsetdatetimeInstances =
+    new Show[OffsetDateTime] with Order[OffsetDateTime] with Hash[OffsetDateTime] with Eq[OffsetDateTime]{
       override def hash(x: OffsetDateTime): Int = x.hashCode
       override def compare(x: OffsetDateTime, y: OffsetDateTime): Int = x.compareTo(y)
       override def show(x: OffsetDateTime): String = x.format(ISO_OFFSET_DATE_TIME)
+      override def eqv(x: OffsetDateTime, y: OffsetDateTime): Boolean = x.equals(y)
     }
 }
 
--- modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/OffsetDateTimeTests.scala
+++ modules/core/src/test/scala/io/chrisdavenport/cats/time/instances/OffsetDateTimeTests.scala
@@ -1,14 +1,16 @@
 package io.chrisdavenport.cats.time.instances
 
+import java.time.OffsetDateTime
 
 import cats.tests.CatsSuite
+import cats.kernel.laws.discipline.EqTests
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
 import TimeArbitraries._
-import java.time.OffsetDateTime
 import io.chrisdavenport.cats.time.instances.offsetdatetime._
 
 class OffsetDateTimeTests extends CatsSuite {
+  checkAll("OffsetDateTime", EqTests[OffsetDateTime].eqv)
   checkAll("OffsetDateTime", HashTests[OffsetDateTime].hash)
   checkAll("OffsetDateTime", OrderTests[OffsetDateTime].order)
 }
