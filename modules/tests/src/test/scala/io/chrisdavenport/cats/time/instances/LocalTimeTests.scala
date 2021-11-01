9867bc3e1f665aa4ac8e5b06df73fe062614e86c
danicheg <e.danicheg@yandex.ru>
2021-11-01 17:31:31 +0300

    Use MUnit in the LocalTimeTests

--- modules/tests/src/test/scala/io/chrisdavenport/cats/time/instances/LocalTimeTests.scala
+++ modules/tests/src/test/scala/io/chrisdavenport/cats/time/instances/LocalTimeTests.scala
@@ -1,14 +1,13 @@
 package io.chrisdavenport.cats.time.instances
 
-
-import cats.tests.CatsSuite
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
-import java.time.LocalTime
-import io.chrisdavenport.cats.time.instances.localtime._
 import io.chrisdavenport.cats.time.arb.TimeArbitraries._
+import io.chrisdavenport.cats.time.instances.localtime._
+import java.time.LocalTime
+import munit.DisciplineSuite
 
-class LocalTimeTests extends CatsSuite {
+class LocalTimeTests extends DisciplineSuite {
   checkAll("LocalTime", HashTests[LocalTime].hash)
   checkAll("LocalTime", OrderTests[LocalTime].order)
 }
