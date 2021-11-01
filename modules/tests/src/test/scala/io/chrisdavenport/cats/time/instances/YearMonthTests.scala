c63955f5ea00d28ba4b7c77352130cf532346fb8
danicheg <e.danicheg@yandex.ru>
2021-11-01 17:39:59 +0300

    Use MUnit in the YearMonthTests

--- modules/tests/src/test/scala/io/chrisdavenport/cats/time/instances/YearMonthTests.scala
+++ modules/tests/src/test/scala/io/chrisdavenport/cats/time/instances/YearMonthTests.scala
@@ -1,14 +1,13 @@
 package io.chrisdavenport.cats.time.instances
 
-
-import cats.tests.CatsSuite
 import cats.kernel.laws.discipline.HashTests
 import cats.kernel.laws.discipline.OrderTests
-import java.time.YearMonth
-import io.chrisdavenport.cats.time.instances.yearmonth._
 import io.chrisdavenport.cats.time.arb.TimeArbitraries._
+import io.chrisdavenport.cats.time.instances.yearmonth._
+import java.time.YearMonth
+import munit.DisciplineSuite
 
-class YearMonthTests extends CatsSuite {
+class YearMonthTests extends DisciplineSuite {
   checkAll("YearMonth", HashTests[YearMonth].hash)
   checkAll("YearMonth", OrderTests[YearMonth].order)
 }
